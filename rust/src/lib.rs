9e093280f0ccc27492d79df1bd0d1893fd1bf456
Serge Vakulenko <serge.vakulenko@sima.ai>
2022-04-19 18:11:10 -0700

    Read input files as vectors.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,86 +1,75 @@
-use clap::{App, Arg};
-use std::error::Error;
-use std::fs::File;
-use std::io::{self, BufRead, BufReader};
+use std::io::BufRead;
 
-type MyResult<T> = Result<T, Box<dyn Error>>;
+type MyResult<T> = Result<T, Box<dyn std::error::Error>>;
 
 #[derive(Debug)]
 pub struct Config {
-    files: Vec<String>,
-    number_lines: bool,
-    number_nonblank_lines: bool,
+    first_file: String,
+    second_file: String,
+    debug_flag: bool,
 }
 
-// --------------------------------------------------
+//
+// Parse command line arguments and return the Config structure.
+//
 pub fn get_args() -> MyResult<Config> {
-    let matches = App::new("ifcomp")
+    let matches = clap::App::new("ifcomp")
         .version("0.1.0")
-        .author("Ken Youens-Clark <kyclark@gmail.com>")
-        .about("Rust cat")
+        .author("Serge Vakulenko <serge.vakulenko@gmail.com>")
+        .about("Rust version of IFCOMP")
         .arg(
-            Arg::with_name("files")
+            clap::Arg::with_name("first_file")
                 .value_name("FILE")
-                .help("Input file(s)")
-                .multiple(true)
-                .default_value("-"),
+                .help("First input file")
+                .required(true)
         )
         .arg(
-            Arg::with_name("number")
-                .short("n")
-                .long("number")
-                .help("Number lines")
+            clap::Arg::with_name("second_file")
+                .value_name("FILE")
+                .help("Second input file")
+                .required(true)
+        )
+        .arg(
+            clap::Arg::with_name("debug")
+                .long("debug")
+                .help("Print debug info")
                 .takes_value(false)
-                .conflicts_with("number_nonblank"),
-        )
-        .arg(
-            Arg::with_name("number_nonblank")
-                .short("b")
-                .long("number-nonblank")
-                .help("Number non-blank lines")
-                .takes_value(false),
         )
         .get_matches();
 
     Ok(Config {
-        files: matches.values_of_lossy("files").unwrap(),
-        number_lines: matches.is_present("number"),
-        number_nonblank_lines: matches.is_present("number_nonblank"),
+        first_file: matches.value_of("first_file").unwrap().to_string(),
+        second_file: matches.value_of("second_file").unwrap().to_string(),
+        debug_flag: matches.is_present("debug"),
     })
 }
 
-// --------------------------------------------------
+//
+// Run the application.
+//
 pub fn run(config: Config) -> MyResult<()> {
-    for filename in config.files {
-        match open(&filename) {
-            Err(e) => eprintln!("{}: {}", filename, e),
-            Ok(file) => {
-                let mut last_num = 0;
-                for (line_num, line_result) in file.lines().enumerate() {
-                    let line = line_result?;
-                    if config.number_lines {
-                        println!("{:6}\t{}", line_num + 1, line);
-                    } else if config.number_nonblank_lines {
-                        if !line.is_empty() {
-                            last_num += 1;
-                            println!("{:6}\t{}", last_num, line);
-                        } else {
-                            println!();
-                        }
-                    } else {
-                        println!("{}", line);
-                    }
-                }
-            }
-        }
+    println!("ifcomp {} {}", config.first_file, config.second_file);
+    if config.debug_flag {
+        println!("(Enable debug info)");
     }
+
+    let a = lines_from_file(config.first_file);
+    let b = lines_from_file(config.second_file);
+
+    println!("a = {:?}", a);
+    println!("b = {:?}", b);
+
     Ok(())
 }
 
-// --------------------------------------------------
-fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
-    match filename {
-        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
-        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
-    }
+fn lines_from_file(filename: impl AsRef<std::path::Path>) -> MyResult<Vec<String>> {
+    let file = std::fs::File::open(filename)?;
+    let buf = std::io::BufReader::new(file);
+    let lines: Vec<String> = buf.lines()
+//        .map(|item| item.expect("Could not parse line"))
+        .map(|item| item.unwrap())
+//        .expect("Error reading file")
+        .collect();
+    println!("lines = {:?}", lines);
+    Ok(lines)
 }
